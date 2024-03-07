2268a03d4926ba41abf52f3082551644b8eb313e
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-07 21:44:20 +0100

    Adde PrismaModule and ProductsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import {
 import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
