569f1fcb3bb14c15e52f532b723d3dd52051491d
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-07 22:05:01 +0100

    Added GET endpoints for products and orders

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Order } from '@prisma/client';
 
 @Injectable()
 export class OrdersService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Order[]> {
+    return this.prismaService.order.findMany({
+      include: { products: { include: { product: true } }, user: true },
+    });
+  }
+
+  public getById(id: Order['id']): Promise<Order | null> {
+    return this.prismaService.order.findUnique({
+      where: { id },
+      include: { products: { include: { product: true } }, user: true },
+    });
+  }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  NotFoundException,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.productsService.getById(id);
+    if (!book) throw new NotFoundException('Product not found');
+    return book;
+  }
 }
