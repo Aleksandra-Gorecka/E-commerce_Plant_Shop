2af25ba845eb50f6885721c07a74787eb018fd8f
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-09 13:02:19 +0100

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,14 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchProducts } from './redux/productsRedux';
 import { Container } from 'react-bootstrap';
-import Header from './components/views/Header'
-import Footer from './components/views/Footer'
+import Header from './components/views/Header/Header'
+import Footer from './components/views/Footer/Footer'
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import ProductCart from './components/pages/ProductCart/ProductCart';
+import Basket from './components/pages/Basket/Basket';
+import OrderSummary from './components/pages/OrderSummary/OrderSummary';
+import NotFound from './components/pages/NotFound/NotFound';
 
 const App = () => {
 
@@ -13,7 +19,13 @@ const App = () => {
   return (
     <Container>
         <Header />
-          Hello World
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductCart />} />
+          <Route path="/basket" element={<Basket />} />
+          <Route path="/ordersummary" element={<OrderSummary />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
         <Footer />
     </Container>
   );
