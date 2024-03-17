3bcb0d4b92e31ff8994529cfcd71f897a9dbed1d
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-17 20:16:10 +0100

    Added ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { OrdersModule } from './orders/orders.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client', 'build'),
+    }),
     ProductsModule,
     PrismaModule,
     OrdersModule,
