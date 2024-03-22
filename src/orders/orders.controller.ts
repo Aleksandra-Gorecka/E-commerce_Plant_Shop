ae5510ca5370af93417d14def4cb0632b2429c7c
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-03-22 14:13:32 +0100

    Added endpoints for Orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,16 +2,20 @@ import {
   Controller,
   Get,
   Post,
+  Put,
+  Delete,
   Body,
   Param,
   ParseUUIDPipe,
   NotFoundException,
   UseGuards,
+  ConflictException,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrderDTO } from './dtos/create-order.dto';
 import { CartProduct } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
 
 @Controller('orders')
 export class OrdersController {
@@ -41,5 +45,36 @@ export class OrdersController {
     } catch (error) {
       throw error;
     }
+  }
+
+  @Put('/:id')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(AdminAuthGuard)
+  async updateOrder(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() orderData: OrderDTO,
+  ) {
+    if (!(await this.ordersService.getById(id)))
+      throw new NotFoundException('Order not found');
+    try {
+      return this.ordersService.updateOrder(id, orderData);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException('Order not found');
+      }
+      if (error instanceof ConflictException) {
+        throw new ConflictException('Order with the same ID already exists');
+      }
+      throw error;
+    }
+  }
+
+  @Delete('/:id')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(AdminAuthGuard)
+  async deleteOrder(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
+      throw new NotFoundException('Order not found');
+    return this.ordersService.deleteOrder(id);
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Order } from '@prisma/client';
 import { CartProduct } from '@prisma/client';
@@ -42,5 +46,31 @@ export class OrdersService {
       }
       throw error;
     }
+  }
+
+  public async updateOrder(
+    id: string,
+    orderData: Omit<Order, 'id' | 'createdAt'>,
+  ): Promise<Order> {
+    try {
+      return await this.prismaService.order.update({
+        where: { id },
+        data: orderData,
+      });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException('Order not found');
+      }
+      if (error.code === 'P2002') {
+        throw new ConflictException('Order with the same ID already exists');
+      }
+      throw error;
+    }
+  }
+
+  public async deleteOrder(id: string): Promise<Order> {
+    return this.prismaService.order.delete({
+      where: { id },
+    });
   }
 }
